583463ffffc492f4974291cecd3795d0b17b0a68
Walter Arias <walter.arias.aguirre@gmail.com>
2020-06-26 15:53:20 -0500

    .

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -10,17 +10,17 @@
         
         public function index()
         {
-            $this->vista('home/homevista');
+            $this->vista('Home/Homevista');
         }
                 
        
         public function ValidarIngreso()
         {
-            if (isset($_POST['usuario'])&& isset($_POST['password'])) {
+            if (isset($_POST['usuario']) && isset($_POST['password'])) {
                 $usuario= filter_var(trim($_POST['usuario']), FILTER_SANITIZE_STRING);
                 $clave= filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
                 $datos =[
-                    'usuario' => $usuario,
+                    'usuario' => strtolower($usuario),
                     'password' =>  $clave];
                 $datos = $this->usuariomodelo->validarIngreso($datos);
                 echo json_encode($datos);
